db283634b84cd24c7a654a415d2dbe47ab0877bf
Diego Gutemberg Barbosa <64646533+diegobarbosa86@users.noreply.github.com>
2021-10-24 19:18:52 -0300

    loader

--- food website/js/script.js
+++ food website/js/script.js
@@ -9,9 +9,19 @@ menu.onclick = () => {
 window.onscroll = () => {
   menu.classList.remove("fa-times");
   menu.classList.remove("active");
-  if(window.scrollY > 60){
-    document.querySelector('#scroll-top').classList.add('active')
+  if (window.scrollY > 60) {
+    document.querySelector("#scroll-top").classList.add("active");
   } else {
-    document.querySelector('#scroll-top').classList.remove('active')
+    document.querySelector("#scroll-top").classList.remove("active");
   }
 };
+
+function loader() {
+  document.querySelector(".loader-container").classList.add("fade-out");
+}
+
+function fadeOut() {
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut;
