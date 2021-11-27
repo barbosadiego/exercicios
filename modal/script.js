454d1b0eebf1b14e2cd72e47fe89a7cd4074a1b3
Diego Gutemberg Barbosa <64646533+diegobarbosa86@users.noreply.github.com>
2021-11-27 15:39:02 -0300

    update

---
+++ modal/script.js
@@ -0,0 +1,23 @@
+// show modal
+const showModal = (openButton, modalContent)=>{
+  const openBtn = document.getElementById(openButton);
+  modalContainer = document.getElementById(modalContent);
+
+  if(openBtn && modalContainer){
+    openBtn.addEventListener('click', ()=>{
+      modalContainer.classList.add('show-modal')
+    })
+  }
+}
+
+showModal('open-modal','modal-container')
+
+// close modal
+const closeBtn = document.querySelectorAll('.close-modal')
+
+function closeModal(){
+  const modalContainer = document.getElementById('modal-container');
+  modalContainer.classList.remove('show-modal')
+}
+
+closeBtn.forEach(c => c.addEventListener('click',closeModal))
