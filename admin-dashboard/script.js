a4569a96709b9e8e4142591f71e30aea69d72180
Diego Gutemberg Barbosa <64646533+diegobarbosa86@users.noreply.github.com>
2022-01-01 20:18:24 -0300

    update

--- admin-dashboard/script.js
+++ admin-dashboard/script.js
@@ -10,3 +10,27 @@ allSideMenu.forEach((item) => {
     li.classList.toggle("active");
   });
 });
+
+
+//TOGGLE SIDEBAR
+
+const menuBar = document.querySelector('#content nav .bx.bx-menu');
+const sideBar = document.getElementById('sidebar');
+
+menuBar.addEventListener('click', function(){
+  sideBar.classList.toggle('hide')
+})
+
+if(window.innerWidth < 768){
+  sideBar.classList.add('hide');
+} 
+
+const searchButton = document.querySelector('#content nav form .form-input button');
+const searchForm = document.querySelector('#content nav form');
+
+searchButton.addEventListener('click', function(e){
+  if(window.innerWidth < 576){
+    e.preventDefault();
+    searchForm.classList.toggle('show')
+  }
+})
