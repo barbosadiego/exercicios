86e54dce60185776235d94fbda218e6400b297a5
barbosadiego <diego.gutemberg@hotmail.com>
2022-04-30 15:46:22 -0300

    dark-light theme

--- mini-portfolio/js/main.js
+++ mini-portfolio/js/main.js
@@ -17,3 +17,27 @@ tabs.forEach((tab) => {
   })
 })
 
+// dark-light theme
+
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'ri-sun-line';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moom-line' : 'ri-sum-line';
+
+if(selectedTheme){
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+  themeButton.classList[selectedIcon === 'ri-moom-line' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', ()=>{
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme)
+
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon())
+})
